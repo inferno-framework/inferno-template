5d073a91d5cde2671058529febc5bcee8c1c2b7a
Stephen MacVicar <smacvicar@mitre.org>
2025-07-24 10:49:50 -0400

    Update to Inferno Core v1.0.5

--- lib/inferno_template/version.rb
+++ lib/inferno_template/version.rb
@@ -1,4 +1,4 @@
 module InfernoTemplate
   VERSION = '0.0.0'.freeze
-  LAST_UPDATED = '2025-07-21'.freeze # TODO: update next release
+  LAST_UPDATED = '2025-07-24'.freeze # TODO: update next release
 end
